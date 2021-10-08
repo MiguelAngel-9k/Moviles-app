4e924844c8ba3f91f14d58b4619e11dcad195f88
MiguelAngel-9k <66149370+MiguelAngel-9k@users.noreply.github.com>
2021-10-08 09:14:03 -0500

    using savedInstanceState

--- app/src/main/java/com/example/mkx_app/MainActivity.kt
+++ app/src/main/java/com/example/mkx_app/MainActivity.kt
@@ -2,10 +2,43 @@ package com.example.mkx_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+
+    var editEmail:EditText? = null
+    var editPassword:EditText? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        editEmail = findViewById<EditText>(R.id.editEmail)
+        editPassword = findViewById<EditText>(R.id.editPassword)
+
+        var savedEmail = savedInstanceState?.getString("user_email").toString()
+        var savedPassword = savedInstanceState?.getString("user_password").toString()
+
+        this.restoreLogin(savedEmail, savedPassword)
+
+    }
+
+    private fun restoreLogin(email: String?, password: String?){
+       if(email == "null")
+            this.editEmail?.setText("")
+        else
+            this.editEmail?.setText(email)
+
+        if(password == "null")
+            this.editPassword?.setText("")
+        else
+            this.editPassword?.setText(password)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        outState.putString("user_email", this.editEmail?.text.toString())
+        outState.putString("user_password", this.editPassword?.text.toString())
     }
 }
